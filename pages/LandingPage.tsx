5d9e1055dbd5f812bec6907acac4cece3484520a
jones <35585020+fleeced@users.noreply.github.com>
2023-10-25 14:16:15 -0600

    added default fonts and colors

--- pages/LandingPage.tsx
+++ pages/LandingPage.tsx
@@ -4,8 +4,18 @@ import React from "react";
 
 const LandingPage: React.FC = () => {
   return (
-    <>
-    </>
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 p-4">
+      <h1 className="text-5xl font-black mb-6 text-primary-blue">Welcome to Digitote</h1>
+      <h2 className="text-3xl font-semibold max-w-lg text-center mb-4 text-primary-pink">Empowering an industry with the tools
+needed to confidently and efficiently
+grow their business.</h2>
+      <p className="text-lg max-w-lg text-center mb-6 font-visby text-black">
+        Discover the power of our platform and how it can revolutionize the way you do business. Our tools are designed with your success in mind.
+      </p>
+      <button className="bg-primary-cyan hover:bg-primary-cyan-75 text-white font-semibold py-2 px-6 rounded">
+        Get Started
+      </button>
+    </div>
   );
 };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,38 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        'primary-blue': {
+          DEFAULT: '#27388e',
+          '75': '#5769b2',
+          '50': '#889aab',
+          '25': '#bacbcc'
+        },
+        'primary-pink': {
+          DEFAULT: '#c03d96',
+          '75': '#d168b1',
+          '50': '#e394cd',
+          '25': '#f5bfec'
+        },
+        'primary-cyan': {
+          DEFAULT: '#38c6f4',
+          '75': '#6ed1f7',
+          '50': '#a4dcfa',
+          '25': '#d9e8fd'
+        }
+      },
+      fontFamily: {
+        'avenir-black': ['Avenir Black', 'sans-serif'],
+        'avenir-heavy': ['Avenir Heavy', 'sans-serif'],
+        'visby': ['Visby CF Regular', 'sans-serif']
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
 }
+
 export default config
