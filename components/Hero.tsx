29dc8b51c213616f3a93c8409b51a1eaedddecc7
jones <35585020+fleeced@users.noreply.github.com>
2023-11-11 11:46:29 -0700

    hero has 2 tone look with hook

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,19 @@
+// Hero.tsx
 import React from "react";
 
 const Hero: React.FC = () => {
-  return <></>;
+  return (
+    <header className="w-full h-screen flex flex-col opacity-0 animate-fadeIn">
+      <div className="flex-1 flex-col bg-primary-blue flex items-center justify-end">
+        <h1 className="text-5xl font-black mb-4 text-white">
+          In a World Where You Can Be Anything Be
+        </h1>
+      </div>
+      <div className="flex-1 bg-primary-pink flex items-center justify-center">
+        <div className="container mx-auto text-center px-6 md:px-12"></div>
+      </div>
+    </header>
+  );
 };
 
 export default Hero;
