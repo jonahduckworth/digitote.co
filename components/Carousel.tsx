8d75e325d15fe70ed158ea8932bc392e6a0a8d65
jones <35585020+fleeced@users.noreply.github.com>
2023-11-11 16:31:57 -0700

    adjusted carousel

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -15,17 +15,17 @@ const allImages = Array(100).fill(images).flat();
 
 const Carousel: React.FC = () => {
   return (
-    <div className="w-full overflow-hidden h-48 relative">
+    <div className="w-full overflow-hidden relative">
       <div className="flex animate-slide">
         {allImages.map((image, index) => (
           <div
             key={index}
-            className="w-64 h-64 mx-8 flex-shrink-0 relative group"
+            className="w-80 h-80 mx-4 flex-shrink-0 relative group"
           >
             <img
               src={image}
               alt={`Slide ${index}`}
-              className="w-full h-full object-cover "
+              className="w-full h-full object-contain"
             />
           </div>
         ))}
