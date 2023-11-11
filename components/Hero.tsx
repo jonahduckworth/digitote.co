705e2a993e26a16dfa048c435cedb1b21e11e402
jones <35585020+fleeced@users.noreply.github.com>
2023-11-11 13:57:55 -0700

    hero looks good

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,16 +1,37 @@
 // Hero.tsx
-import React from "react";
+
+"use client";
+
+import React, { useState, useEffect } from "react";
 
 const Hero: React.FC = () => {
+  const [offset, setOffset] = useState(0);
+
+  const handleScroll = () => setOffset(window.pageYOffset);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <header className="w-full h-screen flex flex-col opacity-0 animate-fadeIn">
-      <div className="flex-1 flex-col bg-primary-blue flex items-center justify-end">
-        <h1 className="text-5xl font-black mb-4 text-white">
+    <header className="w-full h-screen flex flex-col">
+      <div
+        className="flex-1 bg-primary-blue flex items-center justify-center"
+        style={{ transform: `translateY(${offset * 0.3}px)` }}
+      >
+        <h1 className="text-5xl font-black mb-2 text-white">
           In a World Where You Can Be Anything Be
         </h1>
       </div>
-      <div className="flex-1 bg-primary-pink flex items-center justify-center">
-        <div className="container mx-auto text-center px-6 md:px-12"></div>
+      <div
+        className="flex-1 bg-primary-pink flex items-center justify-center"
+        style={{ transform: `translateY(-${offset * 0.3}px)` }}
+      >
+        <p className="text-lg text-white">
+          2-3 sentences that explains what Digitote is.
+        </p>
       </div>
     </header>
   );
