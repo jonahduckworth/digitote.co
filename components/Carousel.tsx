3d54b765d60e5c83636062262951ae4ddd13c767
jones <35585020+fleeced@users.noreply.github.com>
2023-11-18 16:44:20 -0700

    updated hero and carousel

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -4,19 +4,55 @@
 
 import React from "react";
 
-const images = [
-  "images/carousel1.jpg",
-  "images/carousel2.jpg",
-  "images/carousel3.jpg",
-];
+import { motion } from "framer-motion";
 
-// Repeat the images multiple times to fill the carousel
-const allImages = Array(100).fill(images).flat();
+// Function to generate image URLs
+const generateImageUrls = (totalImages: number) => {
+  const images = [];
+  for (let i = 1; i <= totalImages; i++) {
+    images.push(`images/carousel${i}.jpg`);
+  }
+
+  // Fisher-Yates (Knuth) Shuffle
+  for (let i = images.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [images[i], images[j]] = [images[j], images[i]];
+  }
+
+  return images;
+};
 
 const Carousel: React.FC = () => {
+  // Generate and duplicate images
+  const images = generateImageUrls(27);
+  const allImages = [...images, ...images];
+
+  // Assuming each image is 320px wide (80 width + 2 * 4px margin)
+  // Adjust this based on your actual image and margin sizes
+  const singleSetWidth = images.length * 320;
+
+  // Animation settings (customize as needed)
+  const variants = {
+    animate: {
+      x: [0, -singleSetWidth], // Move from 0 to negative one set width
+      transition: {
+        x: {
+          repeat: Infinity,
+          duration: 96, // Shorter duration for faster movement
+          ease: "linear",
+        },
+      },
+    },
+  };
+
   return (
     <div className="w-full overflow-hidden relative">
-      <div className="flex animate-slide">
+      <motion.div
+        className="flex"
+        variants={variants}
+        initial="start"
+        animate="animate"
+      >
         {allImages.map((image, index) => (
           <div
             key={index}
@@ -29,7 +65,7 @@ const Carousel: React.FC = () => {
             />
           </div>
         ))}
-      </div>
+      </motion.div>
     </div>
   );
 };
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,11 +26,24 @@ const Hero: React.FC = () => {
         </h1>
       </div>
       <div
-        className="flex-1 bg-primary-pink flex items-center justify-center"
+        className="flex-1 flex-col bg-primary-pink flex items-start justify-center  px-52"
         style={{ transform: `translateY(-${offset * 0.3}px)` }}
       >
-        <p className="text-lg text-white">
-          2-3 sentences that explains what Digitote is.
+        <h3 className="text-2xl font-bold mb-8 font-avenir-heavy">
+          Where Talent Management Meets Technology
+        </h3>
+        <p className="text-xl text-white mb-8">
+          Welcome to your Digital Tote-bag! A catch all for campaign data,
+          communication, calendars, payments, and so much more. Born from
+          real-life challenges as a talent manager and a relentless pursuit of
+          transparency, Digitote is your ultimate partner in streamlining brand
+          collaborations. We intimately understand the nuances of this job, so
+          we created a platform that supports and systemizers life between
+          talent and manager. We go as far as to say, it’s life changing.
+        </p>
+        <p className="text-xl text-white">
+          Next time your talent is looking for campaign details, tell them,
+          “it’s in your tote!”
         </p>
       </div>
     </header>
