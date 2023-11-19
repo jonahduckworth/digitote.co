cac68fbae21105fbde61e49355fbe29e9bdc0f2b
jones <35585020+fleeced@users.noreply.github.com>
2023-11-18 17:05:31 -0700

    prod features

--- components/Features.tsx
+++ components/Features.tsx
@@ -7,58 +7,46 @@ import React from "react";
 const Features: React.FC = () => {
   const featureData = [
     {
-      icon: "icons/add_note.png",
-      title: "Add Note",
+      icon: "icons/note.png", // Represents project management and organization
+      title: "Centralized Project Management",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "Digitote offers a unified platform to manage all aspects of influencer campaigns. From creative briefs to contract details, track every element of your project in one accessible location. Projects easily organized and filtered by talent or keyword search.",
     },
     {
-      icon: "icons/calendar.png",
-      title: "Calendar",
+      icon: "icons/edit_note.png", // Represents detailed pages for drafting and organizing content
+      title: "Detailed Deliverable Pages",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "From concepts to scripts, draft content to live content, our highly detailed deliverables page keeps all your content in one place. Our attachments feature makes it easy to drag and drop content and keep everything on the platform for easy reference. The ability to add in due dates also keeps everyone on track and on time.",
     },
     {
-      icon: "icons/camera.png",
-      title: "Camera",
+      icon: "icons/phone.png", // Suggests communication and CRM integration
+      title: "Integrated CRM with Brand Representatives",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "Digitote features a CRM system, designed to bridge connections between companies and brand representatives. This integration keeps all brand and company contacts in one place with ease to add to campaigns.",
     },
     {
-      icon: "icons/edit_note.png",
-      title: "Edit Note",
+      icon: "icons/calendar.png", // Indicates an overview, like a dashboard
+      title: "Project Dashboard",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "See all your current campaigns in one place with our project dashboard, you can filter and sort projects by status.",
     },
     {
-      icon: "icons/email.png",
-      title: "Email",
+      icon: "icons/image.png", // Symbolizes user-friendly interface
+      title: "Easy to Use Interface",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "We believe in simplicity and efficiency. Our platform is designed with an intuitive interface, ensuring that campaign management is straightforward and user-friendly. Experience seamless navigation and effortless campaign coordination with Digitote's streamlined interface.",
     },
     {
-      icon: "icons/image.png",
-      title: "Image",
+      icon: "icons/camera.png", // Appropriate for storing media
+      title: "Document and Media Storage",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "Digitote provides secure and organized storage for all campaign-related documents and media. Easily upload, categorize, and access contracts, creative content, and other essential files in one centralized location.",
     },
     {
-      icon: "icons/money.png",
-      title: "Money",
+      icon: "icons/money.png", // Relevant for financial breakdowns
+      title: "Comprehensive Fee Breakdown",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    },
-    {
-      icon: "icons/note.png",
-      title: "Note",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    },
-    {
-      icon: "icons/phone.png",
-      title: "Phone",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "Fundamental to Digitote's ethos is a commitment to transparency. We provide a detailed fee analysis for every project, ensuring talent, talent managers and agencies have a clear understanding of their earnings. This feature displays a comprehensive breakdown of agency fees alongside talent remuneration, fostering trust and clarity in financial dealings.",
     },
   ];
 
