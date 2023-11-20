291a359edba0ea30c926e68caebf168b0d0ba44a
jones <35585020+fleeced@users.noreply.github.com>
2023-11-20 13:35:48 -0700

    bug fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -22,7 +22,7 @@ const Navbar: React.FC = () => {
     <nav className="absolute top-0 left-0 w-full bg-transparent z-10">
       <div className="container mx-auto py-6 flex flex-col md:flex-row items-center justify-between">
         {/* Mobile Menu Icon */}
-        <div className="md:hidden fixed top-0 left-0 mt-6 ml-6">
+        <div className="md:hidden absolute top-0 left-0 mt-6 ml-6">
           <button onClick={toggleNav} className="text-white text-3xl">
             ☰
           </button>
@@ -45,45 +45,52 @@ const Navbar: React.FC = () => {
         {/* Center-aligned links for Desktop and Mobile */}
         <AnimatePresence>
           {(isNavOpen || window.innerWidth >= 768) && (
-            <motion.div
-              className="md:w-full md:h-1/2 md:text-center lg:ml-32 bg-primary-blue md:bg-transparent flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-14 py-4 md:py-0"
-              initial="closed"
-              animate="open"
-              exit="closed"
-              variants={navVariants}
-              transition={{ type: "tween" }}
-            >
-              <a
-                href="/features"
-                className="text-white hover:text-primary-pink rounded-md text-sm font-medium"
+            <>
+              <div className="md:hidden fixed top-0 left-0 mt-6 ml-6">
+                <button onClick={toggleNav} className="text-white text-3xl">
+                  ☰
+                </button>
+              </div>
+              <motion.div
+                className="md:w-full md:h-1/2 md:text-center lg:ml-32 bg-primary-blue md:bg-transparent flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-14 py-4 md:py-0"
+                initial="closed"
+                animate="open"
+                exit="closed"
+                variants={navVariants}
+                transition={{ type: "tween" }}
               >
-                FEATURES
-              </a>
-              <a
-                href="/pricing"
-                className="text-white hover:text-primary-pink rounded-md text-sm font-medium"
-              >
-                PRICING
-              </a>
-              <a
-                href="/"
-                className="font-bold text-2xl text-white whitespace-nowrap"
-              >
-                D I G I T O T E
-              </a>
-              <a
-                href="/about"
-                className="text-white hover:text-primary-pink rounded-md text-sm font-medium"
-              >
-                ABOUT
-              </a>
-              <a
-                href="/contact"
-                className="text-white hover:text-primary-pink rounded-md text-sm font-medium"
-              >
-                CONTACT
-              </a>
-            </motion.div>
+                <a
+                  href="/features"
+                  className="text-white hover:text-primary-pink rounded-md text-sm font-medium"
+                >
+                  FEATURES
+                </a>
+                <a
+                  href="/pricing"
+                  className="text-white hover:text-primary-pink rounded-md text-sm font-medium"
+                >
+                  PRICING
+                </a>
+                <a
+                  href="/"
+                  className="font-bold text-2xl text-white whitespace-nowrap"
+                >
+                  D I G I T O T E
+                </a>
+                <a
+                  href="/about"
+                  className="text-white hover:text-primary-pink rounded-md text-sm font-medium"
+                >
+                  ABOUT
+                </a>
+                <a
+                  href="/contact"
+                  className="text-white hover:text-primary-pink rounded-md text-sm font-medium"
+                >
+                  CONTACT
+                </a>
+              </motion.div>
+            </>
           )}
         </AnimatePresence>
 
@@ -96,15 +103,6 @@ const Navbar: React.FC = () => {
               className="bg-primary-cyan w-32 hover:bg-primary-cyan-75 text-white px-4 py-2 rounded-full text-sm font-medium"
             >
               Book a Demo!
-            </button>
-          </div>
-        )}
-
-        {/* Menu button for small screens (always visible at top left) */}
-        {window.innerWidth < 768 && (
-          <div className="md:hidden fixed top-0 left-0 mt-6 ml-6">
-            <button onClick={toggleNav} className="text-white text-3xl">
-              ☰
             </button>
           </div>
         )}
